24bff689786ddbb45162d03808ffb002cc28f309
longrifle79 <studlee752@yahoo.com>
2025-04-30 16:47:55 -0700

    Math 260 added

---
+++ IT140/Module1/GEwing_Mod1.py
@@ -0,0 +1,11 @@
+user_num = int(input('Enter integer:\n'))
+
+# Type your code here
+
+print("you entered: ", user_num)
+print(user_num, "squared is ", user_num*user_num)
+print("And ", user_num, " cubed is ", user_num^4)
+print("Enter another integer: ")
+another_int = int(input())
+print(user_num, " + ", another_int, "is ", user_num + another_int)
+print(user_num, " * ", another_int, "is ", user_num * another_int)
