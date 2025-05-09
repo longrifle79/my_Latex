7e232854355f0abe92a6a46df6a05ea3e73aa8e2
longrifle79 <alwaysfaithful06@gmail.com>
2025-05-08 20:57:16 -0700

    added module 140

---
+++ IT140/Module2/NameAge.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+user_name = input('What is your name? ')  
+user_age = int(input('How old are you? '))  
+current_year = 2025
+birth_year = current_year - user_age
+print(f'Hello {user_name}! You were born in {birth_year}.')
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
